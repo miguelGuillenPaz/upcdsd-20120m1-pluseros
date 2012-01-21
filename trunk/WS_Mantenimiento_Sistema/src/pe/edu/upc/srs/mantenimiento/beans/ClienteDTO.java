c2b658886b6655162be22ab20f7bc3b42fe71a2a
migue0503@gmail.com <migue0503@gmail.com@b30a831b-4773-0364-46b3-a28dfd2db1f9>
2012-01-21 03:52:09 +0000

    Cambios para el mantenimiento de clientes

---
+++ trunk/WS_Mantenimiento_Sistema/src/pe/edu/upc/srs/mantenimiento/beans/ClienteDTO.java
@@ -0,0 +1,97 @@
+/**
+ *  @author Miguel Guillén 
+*/
+
+package pe.edu.upc.srs.mantenimiento.beans;
+
+import java.io.Serializable;
+
+public class ClienteDTO {
+	private int id;
+	private String codigo;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String nombres;
+	private String direccion;
+	private String telefono;
+	private String email;
+	private String usuario;
+	private String clave;
+	private String rutaFoto;
+	private int id_distrito;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getRutaFoto() {
+		return rutaFoto;
+	}
+	public void setRutaFoto(String rutaFoto) {
+		this.rutaFoto = rutaFoto;
+	}
+	public int getId_distrito() {
+		return id_distrito;
+	}
+	public void setId_distrito(int id_distrito) {
+		this.id_distrito = id_distrito;
+	}
+	
+	
+
+}
---
+++ trunk/WS_Mantenimiento_Sistema/src/pe/edu/upc/srs/mantenimiento/despachadores/IDespachadorCliente.java
@@ -0,0 +1,16 @@
+/**
+ *@author Miguel Guillén
+**/
+
+package pe.edu.upc.srs.mantenimiento.despachadores;
+
+import java.util.ArrayList;
+import pe.edu.upc.srs.mantenimiento.beans.ClienteDTO;;
+
+public interface IDespachadorCliente {
+	
+	
+	public int registrarCliente(ClienteDTO cliente);
+	
+	
+}
