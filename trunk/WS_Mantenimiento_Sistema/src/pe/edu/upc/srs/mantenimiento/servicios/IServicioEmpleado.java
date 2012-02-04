5ab64b35f4173ecaea829ac0ce3218d22bf6ee78
garcia.eliasw@gmail.com <garcia.eliasw@gmail.com@b30a831b-4773-0364-46b3-a28dfd2db1f9>
2012-02-04 17:56:08 +0000

    Servicios Empleado (interfaz y servicio registrar Empleado)

---
+++ trunk/WS_Mantenimiento_Sistema/src/pe/edu/upc/srs/mantenimiento/servicios/IServicioEmpleado.java
@@ -0,0 +1,12 @@
+/**
+ * @author Elías García
+ */
+
+package pe.edu.upc.srs.mantenimiento.servicios;
+
+import pe.edu.upc.srs.mantenimiento.beans.EmpleadoDTO;
+
+public interface IServicioEmpleado {
+	public int registarEmpleado(EmpleadoDTO empleado);
+
+}
